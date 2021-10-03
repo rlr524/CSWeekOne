85b66f5e5a7e5d462d46b1ba976204b3403a7ac3
Rob Ranf <rob@emiyaconsulting.com>
2021-10-03 13:41:14 -0700

    completed week one assignments

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,17 +4,6 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.print("Hello, " +
-                "Madison. ");
-        System.out.println("You are waiting for " + args[0] + ".");
-        System.out.println("I hope you're doing well.");
-        helloTen();
-    }
-
-    private static void helloTen()
-    {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello, World");
-        }
+        System.out.println("Hello, World!");
     }
 }
